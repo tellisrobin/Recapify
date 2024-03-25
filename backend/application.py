23181f3cd4a3692e61a81ff33321aee115f845b3
Robin Tellis <97566347+tellisrobin@users.noreply.github.com>
2024-03-25 18:22:42 -0400

    Home page changes

--- backend/application.py
+++ backend/application.py
@@ -8,7 +8,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 uri = "mongodb+srv://root:root@cluster0.663wj70.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
+#uri = "mongodb+srv://akashku95:gomongodb@customersdata.5pnb9iq.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client['cluster0']
--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,11 +1,15 @@
 import React, {useState,useEffect} from 'react'
 import Navbar from './Navbar'
 import Auth from './Auth'
+import File from './File'
 
 const Home = () => {
     const [user, setUser] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [file, setFile] = useState(null);
+    const [summarizedText,setSummarizedText] = useState("Test");
     useEffect(() => {
+       
        const loggedInUser = localStorage.getItem("user");
        //console.log("loggedInUser:", loggedInUser);
          if (loggedInUser) {
@@ -20,7 +24,32 @@ const Home = () => {
         {isLoading ? (
                 <div></div>
             ) : (
-                user ? <Navbar /> : <Auth />
+                user ? <>
+                <Navbar />
+                <div className=' text-center px-5 py-5 align-middle m-5 border border-y-cyan-50 rounded '>
+                    <input type="file" className='w-full' />
+                </div>
+                
+                <button className='mt-1 mb-1' >
+                        Summarize
+                </button>
+                <div className='text-center  m-3'>
+                    <textarea 
+                        rows={10}
+                        cols={60}
+                        readOnly
+                        className="w-full"
+                    />
+                </div>
+                {/* Save and Discard buttons */}
+            {summarizedText && (
+                <div>
+                    <button className='m-1'>Save</button>
+                    <button className='m-1'>Discard</button>
+                </div>
+            )}
+                </>
+                : <Auth />
             )}
         </div>
   )
